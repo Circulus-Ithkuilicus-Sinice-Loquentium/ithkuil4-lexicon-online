1e02a6f402be50eeca8a9a23e73dc65539f5125d
lumingrui <lomirus@qq.com>
2023-03-22 18:56:38 +0800

    Print message when dir lexion-json has existed

--- scripts/init_lexion.sh
+++ scripts/init_lexion.sh
@@ -7,8 +7,10 @@ fi
 
 # Generate `lexion.json``
 mkdir -p packages && cd packages
-if [ ! -d "lexion-json" ] 
+if [ -d "lexion-json" ] 
 then
+    echo "Directory \"lexion-json\" has existed. Skipped \"git clone\"."
+else
     git clone --depth 1 https://github.com/Circulus-Ithkuilicus-Sinice-Loquentium/lexion-json.git
 fi
 cd lexion-json
