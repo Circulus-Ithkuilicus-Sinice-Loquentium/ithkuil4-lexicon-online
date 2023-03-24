fb9413614212cacb04db0dd56a98f670b81b5db6
lumingrui <lomirus@qq.com>
2023-03-24 18:34:17 +0800

    Update `init_lexicon` script

--- scripts/init_lexicon.sh
+++ scripts/init_lexicon.sh
@@ -14,10 +14,10 @@ else
     git clone --depth 1 https://github.com/Circulus-Ithkuilicus-Sinice-Loquentium/lexicon-json.git
 fi
 cd lexicon-json
-deno task minify
+deno task json
 
 # Copy the `lexicon.json`
 cd ../..
 mkdir -p src/data
-cp packages/lexicon-json/output/minified.json src/data/lexicon.ts
+cp packages/lexicon-json/output/lexicon.json src/data/lexicon.ts
 sed -i '1h;1!H;$!d;x;s/.*/const lexicon: { root: string, refers: string, stems?: [ { "BSC": string, "CTE": string, "CSV": string, "OBJ": string } | string, { "BSC": string, "CTE": string, "CSV": string, "OBJ": string } | string, { "BSC": string, "CTE": string, "CSV": string, "OBJ": string } | string ], notes?: string }[] = &; export default lexicon;/' src/data/lexicon.ts
