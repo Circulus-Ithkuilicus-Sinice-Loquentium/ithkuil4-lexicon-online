aa72ae3ad2acbdac6f3c9f3fa7385de423ec5b3e
lumingrui <lomirus@qq.com>
2023-03-26 17:12:10 +0800

    Update searching algorithm

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,15 +3,38 @@ import { Root } from '../types';
 
 export function search(keyword: string): Root[] {
     const lowerCaseKeyword = keyword.toLowerCase()
-    const list: Root[] = [];
+
+    const rootsFromRoot: Root[] = [];
+    const rootsFromRefers: Root[] = [];
+    const rootsFromStems: Root[] = [];
+
     for (let i = 0; i < lexicon.length; i++) {
-        if (list.length === 10) break;
+        if (rootsFromRoot.length === 10) break;
+
         const { root, refers, stems, notes } = lexicon[i];
-        if (
-            root.toLowerCase().includes(lowerCaseKeyword) ||
-            (refers ? refers.toLowerCase().includes(lowerCaseKeyword) : false)
-        ) {
-            list.push({
+        if (root.toLowerCase().includes(lowerCaseKeyword)) {
+            rootsFromRoot.push({
+                root,
+                refers: refers ? refers.split(' / ') : [],
+                stems,
+                notes
+            })
+        }
+        else if (refers?.toLowerCase().includes(lowerCaseKeyword)) {
+            rootsFromRefers.push({
+                root,
+                refers: refers ? refers.split(' / ') : [],
+                stems,
+                notes
+            })
+        } else if (stems?.some(stem => {
+            if (typeof stem === 'string') {
+                return stem.toLowerCase().includes(lowerCaseKeyword)
+            } else {
+                return Object.values(stem).some(spec => spec.toLowerCase().includes(lowerCaseKeyword))
+            }
+        })) {
+            rootsFromStems.push({
                 root,
                 refers: refers ? refers.split(' / ') : [],
                 stems,
@@ -19,5 +42,11 @@ export function search(keyword: string): Root[] {
             })
         }
     }
-    return list;
+
+    if (rootsFromRoot.length === 10) return rootsFromRoot;
+
+    const roots = rootsFromRoot.concat(rootsFromRefers)
+    if (roots.length >= 10) return roots.slice(0, 10);
+    
+    return roots.concat(rootsFromStems).slice(0, 10);
 }
